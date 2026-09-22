d4ea6bc7b672440d6d589ad1bf98aa57a146abf1
cg408 <k0951221@gmail.com>
2026-09-22 17:49:41 +0800

    2026/09/22

--- First.js
+++ First.js
@@ -1,4 +1,29 @@
 var weight=60;
 var height=160;
+
+var readline = require("readline-sync");
+
+var weight = readline.question("Please enter your weight in (10~200kg): ");
+var height = readline.question("Please enter your height in (10~200cm): ");
+
+while(true){
+    weight = readline.questionfloat("Please enter your weight in (10~200kg): ");
+if(weight < 10 || weight > 200)
+    console.log("Plase 10~200!"){
+    continue;
+}else
+    break;
+
+
+
 var bmi = weight/((height/100)**2);
-console.log("Hello! Your BMI value is "+ bmi)
+console.log("Hello! Your BMI value is "+ bmi);
+
+function test(weight=70){     
+    var weight=50;     
+    console.log("weight="+weight)
+    return weight;
+ }  
+var result=test(90);
+
+  console.log("weight="+weight)
